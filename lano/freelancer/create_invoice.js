d6586f580b4f2f2744b685d8f92b33b088ab631c
nabeel247 <nabeel.pkp@gmail.com>
2022-07-25 21:42:22 +0200

    create invoice test case

--- lano/freelancer/create_invoice.js
+++ lano/freelancer/create_invoice.js
@@ -1,6 +1,6 @@
-describe('Login test case', () => {
+describe('Invoice creation', () => {
 
-    it('freelancer creates an invoice', () => {
+    it('Create and submit an invoice', () => {
         //cy.viewport(1280, 720)
         cy.visit('https://staging.lano.io/en/login')
         cy.get('#user_email').type('nabeel.ahmed+1578@lano.io')
@@ -8,7 +8,7 @@ describe('Login test case', () => {
         cy.contains('Sign in').click()
     
 
-        // click invoice 
+        // Goto invoices
         cy.get('#side-menu > li:nth-child(7) > a').click()
 
         // click 'add invoice' button
@@ -17,51 +17,58 @@ describe('Login test case', () => {
         // click add client
         cy.get('div.add_client.invoice-add-client.invoice-page--add-client').click()
 
-        // find n click a real client
+        // find & click a real client
         cy.contains('beyond').click()
 
         // Click select button for detached invoice 
         cy.get('[data-invoice-type="detached"]').click() 
 
-        // Click add item button 
+        // Click 'add item' button 
         cy.get('#new_invoice > div.quick-invoice--form-layout.m-b-lg > div.current-page.invoice-page.quick-invoice--page.shadowed.white-bg > div.pdf-wrapper.pdf-body.quick-invoice--body > div > div:nth-child(4) > div:nth-child(5) > a > span').click()
 
-        // Add position title
+        // Add a position title
         cy.get('[data-target="inline-edit.mainInput invoices--position-form.titleInput"]').type('Test position 1')
 
         
-        cy.get('[data-target="invoices--position-form.startsOnInput"]').click({
-            force: true
-          });
-
+        // get the current date
         var currentDate = new Date();
         var day = currentDate.getDate();
         cy.log(day)
 
+        // click on start date field
+        cy.get('[data-target="invoices--position-form.startsOnInput"]').click({
+            force: true
+          })
+        
+        // select start date
         cy.contains('.datepicker-dropdown > .datepicker-days > .table-condensed > tbody > tr > td', day).click()
         
-
+        // click on end date field
         cy.get('[data-target="invoices--position-form.endsOnInput"]').click({
           force: true
-        });
+        })
 
+        // select end date
         cy.contains('.datepicker-dropdown > .datepicker-days > .table-condensed > tbody > tr > td', day).click()
 
         // Add price
         cy.get('[data-target="invoices--position-form.rateInput"]').type('20')
 
+        // click on 'currecny' field
         cy.get('div.selectize-control.form-field__control.select.required.single').click()
-        cy.contains('div.selectize-input.items.has-options.full.has-items.focus.input-active.dropdown-active', 'CZK').click()
-
-        //cy.get('.btn-border-radius').click()
         
-       // cy.get('.in > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click()
+        // select currency 'EUR'
+        cy.contains('[data-value]', 'EUR').click()
 
-
+        // click on 'create & submit' button
+        cy.get('.btn-border-radius').click()
+        
+        // // click on 'create & submit' button for final confirmation step
+        cy.get('.in > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click()
 
       })
 
-
+    
     Cypress.on('uncaught:exception', (err, runnable) => {
         // returning false here prevents Cypress from
         // failing the test
